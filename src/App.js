86fbcb8dcf2c6c4d8362a1bdf6a4159f95236a58
Sally Siu <sally.siu4149@gmail.com>
2018-12-04 17:12:08 -0500

    api problems

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
 import React, { Component } from 'react';
 import './App.scss';
-import axios from "axios";
 
 // Import Components
 import Search from "./components/Search.js";
 
 
 class App extends Component {
-	constructor(){
-		super();
-		this.state = {
-			placeQuery: ""
-		}
-	}
+	
 
-	getLocation = () => {
-		axios.get("http://www.mapquestapi.com/geocoding/v1/address", {
-			params: {
-				key: "kwlpGO2AgMDxz3W5HXBulygVAPUK6m8r",
-				location: placeQuery,
-				// maxResults: 1,
-				outFormat: 'json',
-			}
-		}).then((res) => {
-			console.log(res)
-			console.log('muffin')
-		})
-	}
-
-	componentDidMount() {
-		this.getLocation();
-		console.log(`I'm alive!`)
-	}
 
 
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,19 +1,84 @@
 import React, { Component } from "react";
 import EarthPhotos from "./EarthPhotos";
+import axios from "axios";
+import apiKeys from '../data/secrets';
+import LocationSearchInput from "./Autocomplete"
+
+
+
 
 
 class Search extends Component {
+  constructor() {
+    super();
+    this.state = {
+      placeQuery: ""
+    }
+  }
+
+
+  getLocation = () => {
+    axios.get("https://maps.googleapis.com/maps/api/geocode/json", {
+      params: {
+        key: apiKeys.googlemaps,
+        outputFormat: 'json',
+        address: this.state.placeQuery,
+        // location: "Toronto,ON",
+      }
+    }).then((res) => {
+      console.log(res)
+      // console.log('muffin')
+    })
+  }
+
+  componentDidMount() {
+    console.log(`I'm alive!`)
+
+
+
+  }
+
+
+
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hi')
+
+    this.getLocation();
+
+    // this.setState({
+    //   placeQuery: ""
+    // })
+  }
+
+
+
   render() {
     return (
       <main>
         <h2>Hello!!</h2>
         <p>Mapquest is so grown up wow</p>
-        <form action="">
+        <form action="" onSubmit={this.handleSubmit}>
           <label
-            htmlFor="userContinent">What continent would you like to visit on Earth?</label>
-          <input
-            id="userContinent"
-            type="text" />
+            htmlFor="placeQuery">What continent would you like to visit on Earth?</label>
+          {/* <input
+            id="placeQuery"
+            type="text"
+            className="Autocomplete"
+            /> */}
+          <PlacesAutocomplete onChange={this.handleChange} value={this.state.placeQuery}/>
+
+
+
+
+
           <label
             htmlFor="userLang">What languages are you interested in?</label>
           <input
@@ -24,7 +89,25 @@ class Search extends Component {
           <input
             id="userWeather"
             type="text" /> 
+          <input type="submit" value="Submit"/>
+          
         </form>
+
+
+      <script>
+
+        {
+
+        
+  }
+
+
+      </script>
+
+
+
+        <script type="text/javascript" src={`https://maps.googleapis.com/maps/api/js?key=${apiKeys.googlemaps}&libraries=places`}></script>
+
         {/* Can pass the longitudinal and latitudinal coordinates as props to EarthPhotos to get destination photo results from Flickr */}
         <EarthPhotos />
       </main>
