12d72404b4a33c566788b207daf7a343fda6b3f0
Sally Siu <sally.siu4149@gmail.com>
2018-12-04 14:51:43 -0500

    axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
 import './App.scss';
+import axios from "axios";
 
 // Import Components
 import Search from "./components/Search.js";
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Search />
-      </div>
-    );
-  }
+	constructor(){
+		super();
+		this.state = {
+			placeQuery: ""
+		}
+	}
+
+	getLocation = () => {
+		axios.get("http://www.mapquestapi.com/geocoding/v1/address", {
+			params: {
+				key: "kwlpGO2AgMDxz3W5HXBulygVAPUK6m8r",
+				location: placeQuery,
+				// maxResults: 1,
+				outFormat: 'json',
+			}
+		}).then((res) => {
+			console.log(res)
+			console.log('muffin')
+		})
+	}
+
+	componentDidMount() {
+		this.getLocation();
+		console.log(`I'm alive!`)
+	}
+
+
+
+	render() {
+		return (
+			<div className="App">
+				<Search />
+			</div>
+		);
+	}
 }
 
 export default App;
